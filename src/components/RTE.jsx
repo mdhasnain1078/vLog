a75fa0ddc1061c671328fa5012e08cd777e33c92
md <hasnainshaikh62479@gmail.com>
2024-03-28 11:25:39 +0530

    complete vlog WebApp

---
+++ src/components/RTE.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {Editor } from '@tinymce/tinymce-react';
+import {Controller } from 'react-hook-form';
+
+
+export default function RTE({name, control, label, defaultValue =""}) {
+  return (
+    <div className='w-full'> 
+    {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+
+    <Controller
+    name={name || "content"}
+    control={control}
+    render={({field: {onChange}}) => (
+      <Editor
+      apiKey='c0nlcalij1dbyz33ly9owfqshapokj8wnowcs1qlqbpstcol'
+      init={{
+        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        tinycomments_mode: 'embedded',
+        tinycomments_author: 'Author name',
+        mergetags_list: [
+          { value: 'First.Name', title: 'First Name' },
+          { value: 'Email', title: 'Email' },
+        ],
+        ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+      }}
+      onEditorChange={onChange}
+      initialValue="Welcome to TinyMCE!"
+    />
+    )}
+    />
+
+     </div>
+  )
+}
---
+++ src/conf/conf.js
@@ -0,0 +1,16 @@
+// const conf = {
+//     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+//     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+//     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+//     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+//     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+// }
+const conf = {
+    appwriteUrl: "https://cloud.appwrite.io/v1",
+    appwriteProjectId: "66011857aaf0f1cd9b1a",
+    appwriteDatabaseId: "660119a71ce263e98a67",
+    appwriteCollectionId: "660119e2d2dc3914d2e9",
+    appwriteBucketId: "66011b8a02f557a0459e",
+}
+
+export default conf
